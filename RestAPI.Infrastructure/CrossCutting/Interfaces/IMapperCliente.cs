d85559d2a058fa8b2be70661ebcaca49ec0d6d50
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-06 21:31:38 -0300

    feat - mapper interafaces (cliente and product)

---
+++ RestAPI.Infrastructure/CrossCutting/Interfaces/IMapperCliente.cs
@@ -0,0 +1,15 @@
+﻿using ApiRest.Domain.Entitys;
+using RestAPI.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestAPI.Infrastructure.CrossCutting.Interfaces
+{
+    public interface IMapperCliente
+    {
+        Cliente MapperDtoToEntity(ClienteDto clienteDto);
+        IEnumerable<ClienteDto> MapperListClientesDto(IEnumerable<Cliente> clientes);
+        ClienteDto MapperEntityToDto(Cliente cliente);
+    }
+}
