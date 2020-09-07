92f4c2b06f20ff9860632760810b0993f5d035de
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-06 21:05:28 -0300

    feat - create aplication interfaces

---
+++ RestAPI.Application/Interfaces/IApplicationServiceCliente.cs
@@ -0,0 +1,15 @@
+﻿using RestAPI.Application.Dtos;
+using System.Collections.Generic;
+
+namespace RestAPI.Application.Interfaces
+{
+    public interface IApplicationServiceCliente
+    {
+        void Add(ClienteDto clienteDto);
+        void Update(ClienteDto clienteDto);
+        void Remove(ClienteDto clienteDto);
+        IEnumerable<ClienteDto> GetAll();
+        ClienteDto GetById(int id);
+
+    }
+}
---
+++ RestAPI.Application/Interfaces/IApplicationServiceProduto.cs
@@ -0,0 +1,15 @@
+﻿using RestAPI.Application.Dtos;
+using System.Collections.Generic;
+
+namespace RestAPI.Application.Interfaces
+{
+    public interface IApplicationServiceProduto
+    {
+        void Add(ProdutoDto produtoDto);
+        void Update(ProdutoDto produtoDto);
+        void Remove(ProdutoDto produtoDto);
+        IEnumerable<ProdutoDto> GetAll();
+        ProdutoDto GetById(int id);
+
+    }
+}
