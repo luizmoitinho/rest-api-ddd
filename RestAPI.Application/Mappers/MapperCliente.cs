2bcb00862cd97467998a311920148db77826f25d
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-06 22:56:28 -0300

    feat - create Mapper in application

---
+++ RestAPI.Application/Mappers/MapperCliente.cs
@@ -0,0 +1,51 @@
+﻿
+using ApiRest.Domain.Entitys;
+using RestAPI.Application.Dtos;
+using RestAPI.Application.Interfaces.Mappers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestAPI.Application.Mappers
+{
+    public class MapperCliente : IMapperCliente
+    {
+
+        public Cliente MapperDtoToEntity(ClienteDto clienteDto)
+        {
+            var cliente = new Cliente()
+            {
+                Id = clienteDto.Id,
+                Nome = clienteDto.Nome,
+                Sobrenome = clienteDto.Sobrenome,
+                Email = clienteDto.Email
+            };
+            return cliente;
+        }
+
+        public ClienteDto MapperEntityToDto(Cliente cliente)
+        {
+            var clienteDto = new ClienteDto()
+            {
+                Id = cliente.Id,
+                Nome = cliente.Nome,
+                Sobrenome = cliente.Sobrenome,
+                Email = cliente.Email
+            };
+
+            return clienteDto;
+
+        }
+
+        public IEnumerable<ClienteDto> MapperListClientesDto(IEnumerable<Cliente> clientes)
+        {
+            var dto = clientes.Select(c => new ClienteDto
+            {
+                Id = c.Id,
+                Nome = c.Nome,
+                Sobrenome = c.Sobrenome,
+                Email = c.Email
+            });
+            return dto;
+        }
+    }
+}
