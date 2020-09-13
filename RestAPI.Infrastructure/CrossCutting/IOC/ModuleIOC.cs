432a67848e3035cfe8518ac58efe7d87918cb3be
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-13 15:24:02 -0300

    feat - create a module of IOC

---
+++ RestAPI.Infrastructure/CrossCutting/IOC/ModuleIOC.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestAPI.Infrastructure.CrossCutting.IOC
+{
+    public class ModuleIOC : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIOC.Load(builder);
+        }
+
+    }
+}
