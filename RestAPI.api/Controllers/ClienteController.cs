7ec7cf38f7d72d2e8364976cb40977dcf8af1717
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-09-13 21:56:21 -0300

    feat - controller cliente with swagger

---
+++ RestAPI.api/Controllers/ClienteController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Application.Dtos;
+using RestAPI.Application.Interfaces;
+
+namespace RestAPI.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+        private readonly IApplicationServiceCliente _applicatonServiceCliente;
+
+        public ClienteController(IApplicationServiceCliente applicationServiceCliente)
+        {
+            _applicatonServiceCliente = applicationServiceCliente;
+
+        }
+
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicatonServiceCliente.GetAll());
+        }
+
+        //GET api/values/S
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_applicatonServiceCliente.GetById(id));
+        }
+
+        //POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody]ClienteDto clienteDto) {
+
+            try
+            {
+                if (clienteDto == null)
+                    return NotFound();
+
+                _applicatonServiceCliente.Add(clienteDto);
+                return Ok("Cliente Cadastro com sucesso!");
+                   
+            }
+            catch(Exception e) {
+                throw e;
+            }
+
+        }
+
+        //PUT api/values/S
+        [HttpPut]
+        public ActionResult Put([FromBody] ClienteDto clienteDto)
+        {
+            try
+            {
+                if (clienteDto == null)
+                    return NotFound();
+
+                _applicatonServiceCliente.Update(clienteDto);
+                return Ok("Cliente atualizado com sucesso!");
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        //Delete api/values/S
+        [HttpDelete()]
+        public ActionResult Delete([FromBody] ClienteDto clienteDto)
+        {
+            try
+            {
+                if (clienteDto == null)
+                    return NotFound();
+
+                _applicatonServiceCliente.Remove(clienteDto);
+                return Ok("Cliente removido com sucesso!");
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+    }
+}
--- RestAPI.api/Startup.cs
+++ RestAPI.api/Startup.cs
@@ -28,16 +28,15 @@ namespace RestAPI.api
             services.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Model DDD", Version = "v1" });   
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Model DDD", Version = "v1" });
             });
-
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
             builder.RegisterModule(new ModuleIOC());
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,10 +46,10 @@ namespace RestAPI.api
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseSwagger();
 
-            app.UseSwaggerUI( c => {
+            app.UseSwaggerUI(c =>
+            {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Model DDD");
             });
 
